784c3c8dc06126ae4e1739dfd18bedd86aeaadfb
Jake Coutry <jacob@perfectsensedigital.com>
2019-11-12 13:44:47 -0500

    creating PLM content type and definitions in root styleguide

---
+++ core/src/main/java/bex/training/core/module/ProductLaunchModule.java
@@ -0,0 +1,11 @@
+package bex.training.core.module;
+
+import brightspot.core.module.ModuleType;
+
+public class ProductLaunchModule extends ModuleType {
+
+    private String title;
+
+    private String description;
+
+}
